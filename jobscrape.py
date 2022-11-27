1c7c3b1e06ffa8521c35e98f465bcbc7f036d840
Daniel Crimp <daniel.crimp@outlook.com>
2022-11-27 21:23:28 +0000

    bodged salary selector

--- jobscrape.py
+++ jobscrape.py
@@ -16,11 +16,12 @@ def get_data(spl):
 
     return mysession, myshid
 
-
+def has_numbers(inputString):
+    # necessary for a bodge later
+    return any(char.isdigit() for char in inputString)
 
 def main():
     
-    out = pd.DataFrame(columns=['jid','title','sal','desc'])
     session, shid = get_data(searchpayload)
     pagenum = 0
     idlist = []
@@ -44,31 +45,37 @@ def main():
     
     print("Search complete, "+ str(len(idlist))+" Jobs found")
 
-
-
-    # next, just need to For loop this for all IDs, extract salary and Job desc, and that should give us a
-    # dataset from which we can do datasciencey stuff.
-
     base="https://www.jobserve.com/gb/en/mob/job/"
+
+    tuplelist = []
 
     for i in idlist:
         r = session.get(base+i)
         mysoup = soup.BeautifulSoup(r.content,'lxml')
         title = mysoup.select_one("#cnt > article > header > h3").text
-        sal = mysoup.select_one('#cnt > article > div > div.jobinfo > div:nth-child(4)').text
+
+        # forgive me for what I am about to do...
+        salparent = mysoup.select_one('#cnt > article > div > div.jobinfo')
+        sal = ""
+        children = salparent.findChildren()
+        for child in children:
+            # oh god...
+            if ("Rate" in child.text) and (has_numbers(child.text)):
+                sal = child.text[12:]
+            
+        #sal = mysoup.select_one('#cnt > article > div > div.jobinfo > div:nth-child(4)').text
         desc = mysoup.select_one('#cnt > article > div > div.jobdesc').text
 
-        mydict = {
-            'jid':i,
-            'title':title,
-            'sal':sal,
-            'desc':desc
-        }
+        mytuple = (i, title.strip(), sal.strip(), desc.strip())
+        tuplelist.append(mytuple)
 
-        ddf = pd.DataFrame([mydict])
+        # yes i'm accumulating a dataframe in a For
+        # I'll convert it to a tuple arrangement later
+        
 
-        out = pd.concat([out, ddf])
+    out = pd.DataFrame(data = tuplelist, columns=['jid','title','sal','desc'])
 
+    out = out.set_index('jid')
     out.to_csv('jobs.csv')
 
 
